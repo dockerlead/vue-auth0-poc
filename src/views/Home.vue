158cc2bf0529a4eacb28247766fd962339f5a7fe
crazyoptimist <crazyoptimist@mail.com>
2020-06-17 18:24:57 -0500

    :tada:Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Farewell msg="Hi, DockerLead!" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Farewell from "@/components/Farewell.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Farewell
   }
 };
 </script>
